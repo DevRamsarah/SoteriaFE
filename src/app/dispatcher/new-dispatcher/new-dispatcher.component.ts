165010a71692a6ddf05d2929ee5489cba7ed93a6
Dev2205 <dhramsarah@student.udm.ac.mu>
2021-02-10 20:28:46 +0400

    dispatch form

--- src/app/dispatcher/new-dispatcher/new-dispatcher.component.ts
+++ src/app/dispatcher/new-dispatcher/new-dispatcher.component.ts
@@ -18,16 +18,46 @@ export class NewDispatcherComponent implements OnInit {
     ID: '6',
     Name: 'rerg',
   }];
+  PostSiteL = [{
+    ID: '3',
+    Name: 'a',
+  }, {
+    ID: '33',
+    Name: 'GasdTX',
+  }, {
+    ID: '63',
+    Name: 'rerg',
+  }];
+  GuardL = [{
+    ID: '17',
+    Name: 'a',
+  }, {
+    ID: '37',
+    Name: 'GasdTX',
+  }, {
+    ID: '67',
+    Name: 'rerg',
+  }];
   constructor() { }
 
   ngOnInit(): void {
     this.New = new FormGroup({
       ClientID: new FormControl(null, [Validators.required]),
+      PostSite: new FormControl(null, [Validators.required]),
+      Guard: new FormControl(null, [Validators.required]),
+      Priority: new FormControl(null, [Validators.required]),
+      Caller: new FormControl(null, [Validators.required]),
+      CallerName: new FormControl(null, [Validators.required]),
+      Incident: new FormControl(null, [Validators.required]),
+      IncidentDate: new FormControl(null, [Validators.required]),
+      IncidentDetail: new FormControl(null, [Validators.required]),
+      ActionTaken: new FormControl(null, [Validators.required]),
+      InternalNotes: new FormControl(null, [Validators.required]),
 
     });
 
   }
   submit() {
-    console.log(this.New)
+    console.log(this.New.value)
   }
 }
