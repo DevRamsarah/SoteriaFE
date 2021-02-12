a3b1ccd0e5fb60991c05d87a6fbd3211ad623418
Dev2205 <dhramsarah@student.udm.ac.mu>
2021-02-12 17:40:22 +0400

    new client form + firebase

--- src/app/client/new-client/new-client/new-client.component.ts
+++ src/app/client/new-client/new-client/new-client.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ClientService } from 'src/services/client/client.service';
 
 @Component({
   selector: 'app-new-client',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-client.component.css']
 })
 export class NewClientComponent implements OnInit {
+  New: FormGroup;
+  GuardL = [{
+    ID: '17',
+    Name: 'a',
+  }, {
+    ID: '37',
+    Name: 'GasdTX',
+  }, {
+    ID: '67',
+    Name: 'rerg',
+  }];
 
-  constructor() { }
+  constructor(public firebaseCrud: ClientService, public router: Router) { }
 
   ngOnInit(): void {
-  }
+    this.New = new FormGroup({
+      ClientName: new FormControl(null, [Validators.required]),
+      ClientEmail: new FormControl(null, [Validators.required]),
+      ContactName: new FormControl(null, [Validators.required]),
+      MobileNum: new FormControl(null, [Validators.required]),
+      PhoneNum: new FormControl(null, [Validators.required]),
+      faxNum: new FormControl(null, [Validators.required]),
+      Category: new FormControl(null, [Validators.required])
 
+
+    });
+  }
+  submit() {
+    console.log(this.New.value)
+    this.firebaseCrud.createNewClient(this.New.value)
+  }
 }
--- src/services/client/client.service.ts
+++ src/services/client/client.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
 
-  constructor() { }
+  constructor(public firebaseCrud: AngularFirestore) { }
+  createNewClient(records) {
+    return this.firebaseCrud.collection('clients').add(records)
+  }
+  getClient() {
+    return this.firebaseCrud.collection('clients').valueChanges({ idField: 'DispatcherID' })
+
+  }
 }
