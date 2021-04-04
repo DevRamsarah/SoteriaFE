df0d13ef956e480c6678c62371a8689725c9d8f7
Dev2205 <dhramsarah@student.udm.ac.mu>
2021-04-04 23:14:41 +0400

    invoice

--- src/app/invoice/add-invoice/add-invoice.component.ts
+++ src/app/invoice/add-invoice/add-invoice.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { ClientService } from 'src/services/client/client.service';
+
 
 @Component({
   selector: 'app-add-invoice',
@@ -9,46 +7,15 @@ import { ClientService } from 'src/services/client/client.service';
   styleUrls: ['./add-invoice.component.css']
 })
 export class AddInvoiceComponent implements OnInit {
-  loading = false;
 
-  New: FormGroup;
-  GuardL = [{
-    ID: '17',
-    Name: 'a',
-  }, {
-    ID: '37',
-    Name: 'GasdTX',
-  }, {
-    ID: '67',
-    Name: 'rerg',
-  }];
-
-  constructor(public firebaseCrud: ClientService, public router: Router) { }
+  invoice = {
+    Title: "",
+    Summary: ""
+  }
+  constructor() { }
 
   ngOnInit(): void {
-    this.New = new FormGroup({
-      ClientName: new FormControl(null, [Validators.required]),
-      ClientEmail: new FormControl(null, [Validators.required]),
-      ContactName: new FormControl(null, [Validators.required]),
-      MobileNum: new FormControl(null, [Validators.required]),
-      PhoneNum: new FormControl(null, [Validators.required]),
-      faxNum: new FormControl(null, [Validators.required]),
-      ClientAddress: new FormControl(null, [Validators.required]),
-      Category: new FormControl(null, [Validators.required])
-
-
-    });
-  }
-  submit() {
-    // console.log(this.New.value)
-    this.loading = true;
-
-    this.firebaseCrud.createNewClient(this.New.value).then(
-      () => {
-        alert("Client Added")// add sweet alert
-      }
-    )
-    this.router.navigate(["Clients"]);
 
   }
+
 }
