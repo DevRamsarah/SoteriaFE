d27f843a8a14b4bdf03f6ca5ecb547eb7fa5c3a8
Dev2205 <dhramsarah@student.udm.ac.mu>
2021-04-04 23:55:42 +0400

    invoice

--- src/app/invoice/add-invoice/add-invoice.component.ts
+++ src/app/invoice/add-invoice/add-invoice.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PostSiteService } from 'src/services/post-site/post-site.service';
 
 
 @Component({
@@ -12,10 +13,24 @@ export class AddInvoiceComponent implements OnInit {
     Title: "",
     Summary: ""
   }
-  constructor() { }
+  clientD = [];
+
+  constructor(public firebaseCrud: PostSiteService) { }
 
   ngOnInit(): void {
+    this.firebaseCrud.getClient().subscribe((clients: any) => {
+      clients.forEach(client => {
+        let clientData: any = {};
+        clientData.id = client.ClientID;
+        clientData.Name = client.ClientName;
+        this.clientD.push(clientData);
 
+
+
+      });
+      console.log(this.clientD);
+
+    })
   }
 
 }
