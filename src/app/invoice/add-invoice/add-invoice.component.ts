0c53f487ad8114ddb9a7f8d8020856a52d0657b4
Dev2205 <dhramsarah@student.udm.ac.mu>
2021-04-05 11:00:31 +0400

    html2pdf

--- src/app/invoice/add-invoice/add-invoice.component.ts
+++ src/app/invoice/add-invoice/add-invoice.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PostSiteService } from 'src/services/post-site/post-site.service';
-
+import * as html2pdf from 'html2pdf.js'
 
 @Component({
   selector: 'app-add-invoice',
@@ -33,4 +33,20 @@ export class AddInvoiceComponent implements OnInit {
     })
   }
 
+  download() {
+    var element = document.getElementById('print');
+    var opt = {
+      margin: 1,
+      filename: 'invoice.pdf',
+      image: { type: 'jpeg', quality: 0.98 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: 'in', orientation: 'portrait' }
+    };
+
+    // New Promise-based usage:
+    html2pdf().set(opt).from(element).save();
+
+    // Old monolithic-style usage:
+    html2pdf(element, opt);
+  }
 }
