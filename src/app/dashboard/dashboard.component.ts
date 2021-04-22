8e32c6c3b39a7aceafb0d85b64f0bac2ff7f1247
DevRamsarah <dhramsarah@student.student.ac.mu>
2021-04-22 21:54:26 +0400

    dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientService } from 'src/services/client/client.service';
+import { GuardService } from 'src/services/guard/guard.service';
+import { PostSiteService } from 'src/services/post-site/post-site.service';
+import { InvoiceService } from 'src/services/invoice/invoice.service';
+import { DispatcherService } from 'src/services/dispatcher/dispatcher.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +11,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  CountClient = 0
+  CountGuard = 0
+  CountPostSite = 0
+  CountInvoice = 0
+  CountDispatch = 0
+  constructor(
+    public ClientD: ClientService,
+    public GuardD: GuardService,
+    public PostSiteD: PostSiteService,
+    public InvoiceD: InvoiceService,
+    public DispatchD: DispatcherService,
+  ) { }
 
   ngOnInit(): void {
+    this.ClientD.getClient().subscribe((Client: any) => {
+      Client.forEach(element => {
+        this.CountClient++;
+      });
+    });
+
+    this.GuardD.getGuard().subscribe((Guard: any) => {
+      Guard.forEach(element => {
+        this.CountGuard++;
+      });
+    });
+
+    this.PostSiteD.getPostSite().subscribe((PostSite: any) => {
+      PostSite.forEach(element => {
+        this.CountPostSite++;
+      });
+    });
+
+    this.DispatchD.getDispatch().subscribe((Dispatch: any) => {
+      Dispatch.forEach(element => {
+        this.CountDispatch++;
+      });
+    });
+
+    this.InvoiceD.getInvoice().subscribe((Invoice: any) => {
+      Invoice.forEach(element => {
+        this.CountInvoice++;
+      });
+    });
   }
 
 }
