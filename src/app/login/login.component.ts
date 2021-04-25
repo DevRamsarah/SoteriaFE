87b1c8650f1b3619e151050edb2492e84cfb9833
DevRamsarah <dhramsarah@student.student.ac.mu>
2021-04-26 02:55:37 +0400

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,38 +10,39 @@ import { AngularFireAuth } from '@angular/fire/auth';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  errMssg=""
-  forget=false
+  errMssg = ""
+  forget = false
   InOut = true;
   loading = false;
   @Output() log: EventEmitter<any> = new EventEmitter();
-  constructor(public firebaseAuth: AngularFireAuth,public firebaseService: FirebaseService, private router: Router) { }
+  constructor(public firebaseAuth: AngularFireAuth, public firebaseService: FirebaseService, private router: Router) { }
   ngOnInit(): void {
 
-    
+
     const url = window.location.href
-    if (this.router.url == "/" && new URLSearchParams(window.location.search).has("apiKey")){
-    this.confirmLog(url)}
+    if (this.router.url == "/" && new URLSearchParams(window.location.search).has("apiKey")) {
+      this.confirmLog(url)
+    }
   }
 
-  async confirmLog(url){
-try {
-  if( this.firebaseAuth.isSignInWithEmailLink(url)){
-    let email= window.prompt('Please provide your email for confirmation')
-    const result = await this.firebaseAuth.signInWithEmailLink(email,url)
-    localStorage.setItem('passwordless',"true")
-    localStorage.setItem('userid', result.user.uid)
-    window.location.href = "Dashboard"
-  }
-} catch (error) {
-  Swal.fire({
-    icon: 'error',
-    title: 'Oops...',
-    text: 'Something went wrong!',
-    footer: error.message
-  })
-}
-    
+  async confirmLog(url) {
+    try {
+      if (this.firebaseAuth.isSignInWithEmailLink(url)) {
+        let email = window.prompt('Please provide your email for confirmation')
+        const result = await this.firebaseAuth.signInWithEmailLink(email, url)
+        localStorage.setItem('passwordless', "true")
+        localStorage.setItem('userid', result.user.uid)
+        window.location.href = "Dashboard"
+      }
+    } catch (error) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Something went wrong!',
+        footer: error.message
+      })
+    }
+
   }
 
   onSignin(email: string, password: string) {
@@ -65,65 +66,76 @@ try {
     })
     this.loading = true;
   }
-  onSignup(email: string, password: string,rpassword: string, name:string) {
-    if(password === rpassword){
-    this.firebaseService.signup(email, password,name).then(() => {
-      Swal.fire({
-        position: 'center',
-        icon: 'success',
-        title: 'Registration successful',
-        showConfirmButton: false,
-        timer: 1500
-      })
-     
-    }).catch(err => {
-      Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'Something went wrong!',
-        footer: err
-      })
-      console.log(err);
-      this.errMssg = err
-      this.loading = false;
+  onSignup(email: string, password: string, rpassword: string, name: string) {
+    //   if(password === rpassword){
+    //   // this.firebaseService.signup(email, password, fname, lname).then(() => {
+    //     Swal.fire({
+    //       position: 'center',
+    //       icon: 'success',
+    //       title: 'Registration successful',
+    //       showConfirmButton: false,
+    //       timer: 1500
+    //     })
 
-    })
-  }else{
+    //   }).catch(err => {
+    //     Swal.fire({
+    //       icon: 'error',
+    //       title: 'Oops...',
+    //       text: 'Something went wrong!',
+    //       footer: err
+    //     })
+    //     console.log(err);
+    //     this.errMssg = err
+    //     this.loading = false;
+
+    //   })
+    // }else{
     Swal.fire({
       icon: 'error',
       title: 'Oops...',
       text: 'Something went wrong!',
-      footer: "Password do not match. Please try again"
+      footer: "No Register Available"
     })
-  }
+    // }
   }
   clickEvent() {
     this.InOut = !this.InOut;
   }
-redirect(){
-  setTimeout(() => {
-    switch (JSON.parse(localStorage.getItem('CurentUser'))[0].role) {
-      case "Admin":
-        window.location.href = "Dashboard"
-        break;
-    case "Client":
+  redirect() {
+    setTimeout(() => {
+      switch (JSON.parse(localStorage.getItem('CurentUser'))[0].role) {
+        case "Admin":
+          window.location.href = "Dashboard"
+          break;
+        case "Client":
           window.location.href = "Location"
           break;
-      default:
-        localStorage.clear();
-    window.location.reload()
-        break;
-    }
-    if(JSON.parse(localStorage.getItem('CurentUser'))[0].role == "Admin"){
-    window.location.href = "Dashboard"
-  }else{}
+        default:
+        case "Guard":
+          Swal.fire({
+            icon: 'warning',
+            title: 'Guard Mail Detected!',
+            text: 'Please use the Mobile app.',
+            footer: "Please contact the admin to resolve this issue."
+          }).then(() => {
+            localStorage.clear();
+            window.location.reload()
+          })
+          break;
+          localStorage.clear();
+          window.location.reload()
+          break;
+      }
+      if (JSON.parse(localStorage.getItem('CurentUser'))[0].role == "Admin") {
+        window.location.href = "Dashboard"
+      } else { }
 
-  }, 4000);
-}
-  forgetP(){
-    this.forget? this.forget=false: this.forget=true
+    }, 4000);
   }
-  resetP(email){
+  forgetP() {
+    this.forget ? this.forget = false : this.forget = true
+  }
+  resetP(email) {
     this.firebaseService.reset(email)
   }
 }
